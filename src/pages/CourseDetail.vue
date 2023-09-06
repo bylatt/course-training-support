ee4475f50726c5bc47f7af2147f5dec787ca682e
Joe <poompanya.puangput@codezyne.com>
2023-09-06 11:24:19 +0700

    Update ui

--- src/pages/CourseDetail.vue
+++ src/pages/CourseDetail.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    <h1>{{ greeting }}</h1>
-    <p>{{ message }}</p>
+      <!-- Course Banner Image -->
+      <img :src="course.imageUrl" alt="Course Banner" class="course-banner">
+
+      <!-- Course Title -->
+      <h1 class="course-title">{{ course.title }}</h1>
+
+      <!-- Course Description -->
+      <p class="course-description">{{ course.description }}</p>
+
+      <!-- Time Period -->
+      <p class="time-period">{{ course.timePeriod }}</p>
+
+      <!-- Yellow Button (Sticky at Bottom) -->
+      <button class="yellow-button">Open Course</button>
   </div>
 </template>
 
@@ -9,19 +21,53 @@
 export default {
   data() {
     return {
-      greeting: 'Hello, Vue!',
-      message: 'Welcome to Vue.js single-file components.'
+      course: {
+        imageUrl: 'https://s3.amazonaws.com/thumbnails.venngage.com/template/b574ec3f-672a-4bc9-9457-4bc7f0e1d982.png', // Replace with your course image URL
+        title: 'Course Title',
+        description: 'Course Description',
+        timePeriod: 'Period: 2 days',
+      },
     };
   }
 };
 </script>
 
 <style scoped>
-/* Scoped CSS specific to this component */
-h1 {
-  color: blue;
+/* Component-specific styles */
+.course-banner {
+  max-width: 100%;
+  height: auto;
 }
-p {
-  font-weight: bold;
+
+.course-title {
+  font-size: 24px;
+  margin-top: 20px;
+  text-align: left;
+}
+
+.course-description {
+  font-size: 16px;
+  margin-top: 10px;
+  text-align: left;
+}
+
+.time-period {
+  font-size: 16px;
+  margin-top: 10px;
+  color: #999; /* Adjust the color as needed */
+  text-align: left;
+}
+
+/* Yellow Button */
+.yellow-button {
+  background-color: #FECE50;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  position: sticky;
+  bottom: 0;
+  margin-bottom: 20px; /* Add some margin for spacing */
 }
 </style>
