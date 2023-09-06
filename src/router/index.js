2df0cebef00449fc32fc4af71e2f28d8fb88adb9
Joe <poompanya.puangput@codezyne.com>
2023-09-06 11:29:13 +0700

    Resolve conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
+import Course from "../pages/Course.vue";
+import Waiting from "../pages/WaitingList.vue";
+import ClassAvailable from "../pages/ClassAvailable.vue";
+
 
 const routes = [
   {
