15fb0db4f29ff8249538ff3a67b004bd82c0f508
Lattapon Yodsuwan <lattapon@bylatt.co>
2023-09-06 11:20:30 +0700

    feat: get courses

--- src/pages/Course.vue
+++ src/pages/Course.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as api from '../api.js'
+onMounted(() => {
+    api.getCourses().then(result => {
+        console.log(result)
+    }).catch(err => {
+        console.log(err)
+    })
+    console.log(api)
+    console.log('mounted')
+})
+</script>
+
 <template>
     <div>Course</div>
 </template>
