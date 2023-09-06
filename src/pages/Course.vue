91ee6a447570d46d9d86939d700d99805c3d89c7
Lattapon Yodsuwan <lattapon@bylatt.co>
2023-09-06 11:44:21 +0700

    commit them

--- src/pages/Course.vue
+++ src/pages/Course.vue
@@ -2,13 +2,8 @@
 import { ref, onMounted } from 'vue'
 import * as api from '../api.js'
 onMounted(() => {
-    api.getCourses().then(result => {
-        console.log(result)
-    }).catch(err => {
-        console.log(err)
-    })
-    console.log(api)
-    console.log('mounted')
+    api.getCourses().then(result => console.log(result)).catch(err => console.log(err))
+    api.getCourse("3ARi1sfUR8K9NrDsYkJm").then(result => console.log(result)).catch(err => console.log(err))
 })
 </script>
 
