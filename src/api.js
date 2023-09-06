8c907e473907e145ad101fae7171f5040ca5e3f9
Lattapon Yodsuwan <lattapon@bylatt.co>
2023-09-06 10:43:14 +0700

    feat(firebase): add firebase client

---
+++ src/api.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDoc, getFirestore, query } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF6JVZVmChlFqQQ9lgPoYgpjzsCNbCXOE",
+  authDomain: "course-training-support.firebaseapp.com",
+  projectId: "course-training-support",
+  storageBucket: "course-training-support.appspot.com",
+  messagingSenderId: "673181277367",
+  appId: "1:673181277367:web:526d1fbf77f715885ec132",
+  measurementId: "G-KZCXW5F74M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function getCourses() {
+  const snapshot = await getDocs(collection(db, "courses"));
+  return snapshot.map((doc) => doc.data());
+}
+
+async function getCourse(id) {
+  const docRef = doc(db, "courses", id);
+  const docSnap = await getDoc(docRef);
+  return docSnap.data();
+}
+
+export default { getCourses, getCourse };
