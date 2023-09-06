db5c9bd7dc6ad09b9250a3db33c6ddf2cde3cb46
Jerapong Nampetch <tong@appsynth.net>
2023-09-06 10:30:23 +0700

    feat: add `courses` route

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
         <router-link to="/">Home</router-link>
       </li>
       <li>
+        <router-link to="/courses">Courses</router-link>
+      </li>
+      <li>
         <router-link to="/about">About</router-link>
       </li>
     </ul>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
+import Course from "../pages/Course.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: "About",
     component: About,
   },
+  {
+    path: "/courses",
+    name: "Courses",
+    component: Course,
+  }
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
