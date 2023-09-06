116f55bf13aeb7f6e2ad04f3faa475bd3353efd1
Sirisub Amnartsirisuk <ko@appsynth.net>
2023-09-06 14:20:33 +0700

    Update css

--- src/pages/Course.vue
+++ src/pages/Course.vue
@@ -1,4 +1,5 @@
 <script setup>
+import "/src/assets/main.css";
 import { ref, onMounted } from "vue";
 import * as api from "../api.js";
 let courses = ref([]);
@@ -21,9 +22,11 @@ onMounted(() => {
   <div v-for="course in courses" class="list-items">
     <img src="{{ course.image }}" alt="Random image" />
     <div class="content">
-      <h3>{{ course.title }}</h3>
-      <p>{{ course.description }}</p>
-      <p><i class="fa-regular fa-clock"></i> {{ course.duration }}</p>
+      <h3 class="title">{{ course.title }}</h3>
+      <p class="description">{{ course.description }}</p>
+      <p class="duration">
+        <i class="fa-regular fa-clock"></i> {{ course.duration }}
+      </p>
       <a href="/courses/{{ course.id }}" target="_blank" class="btn-primary"
         >View Course <i class="fa-solid fa-chevron-right"></i
       ></a>
