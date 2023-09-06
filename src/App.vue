f8ec0bfc03697a4973d860ad8de7f9fac0557a1c
Sirisub Amnartsirisuk <ko@appsynth.net>
2023-09-06 11:14:05 +0700

    Init Waiting page on Home screen

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
       <li>
         <router-link to="/about">About</router-link>
       </li>
+      <li>
+        <router-link to="/waitingList">Waiting list</router-link>
+      </li>
     </ul>
   </div>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
 import Course from "../pages/Course.vue";
+import Waiting from "../pages/WaitingList.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/courses",
     name: "Courses",
     component: Course,
+  },
+  {
+    path: "/waitingList",
+    name: "WaitingList",
+    component: Waiting,
   }
 ];
 
